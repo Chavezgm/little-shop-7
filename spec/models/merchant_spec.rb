46e977b43cba43303a6514298e47e2e2cc875c35
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-29 10:56:41 -0500

    refactor

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Merchant, type: :model do
    end
 
    it "top_5_customers" do
-      @green_merchant = Merchant.create!(name: "Green Inc")
+      green_merchant = Merchant.create!(name: "Green Inc")
       @black_merchant = Merchant.create!(name: "Black Inc")
       @brown_merchant = Merchant.create!(name: "Brown Inc")
       
@@ -19,16 +19,10 @@ RSpec.describe Merchant, type: :model do
 
       @cust_1 = create(:customer, first_name: "Joey", last_name: "Ondricka")
       @cust_2 = create(:customer, first_name: "John", last_name: "Smith")
-      @cust_3 = create(:customer, first_name: "Jane", last_name: "Perry") #cust_4 will have 15 successful transactions 
+      @cust_3 = create(:customer, first_name: "Jane", last_name: "Perry")
       @cust_4 = create(:customer, first_name: "Buzz", last_name: "Lightyeay")
       @cust_5 = create(:customer, first_name: "Patrick", last_name: "Karl")
-      @cust_6 = Customer.create(first_name: "rick", last_name: "l") #cust_6 will have 15 successful transactions 
-      # require 'pry'; binding.pry
-      #each customer that is created will have 5 invoices with it plus the ones we created below for example cust_1 has 7 invoices
-      #for each  invoice it will have 3 transactions
-      # 5 * 3 = 15 transactions from factory when we create a customer
-      #2 * 3 = 6 transaction for eac in voice that we created which was invoice6 and invoice1 
-      #then created 2 extra transactions for customer 1 for each invoice that we made 
+      @cust_6 = Customer.create(first_name: "rick", last_name: "l") 
       invoice1 = FactoryBot.create(:invoice, customer_id: @cust_1.id, status: 1)  #cust1 = 21
       invoice6 = FactoryBot.create(:invoice, customer_id: @cust_1.id, status: 1)
       
@@ -48,7 +42,6 @@ RSpec.describe Merchant, type: :model do
 
       expect(@green_merchant.top_five_customers).to eq([@cust_5,@cust_2,@cust_1,@cust_3,@cust_4])
       expect(@green_merchant.top_five_customers).to_not eq([@cust_6])
-
    end
 
    it "#pending_items" do
